baefe56c86c6ed28367a3ab584e52920d78340aa
Yashraj Patil <yashrajspatil808@gmail.com>
2026-07-22 17:19:00 +0530

    feat(lost-items): implement complete CRUD with authorization

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import lostItemRoutes from "./routes/lostItemRoutes.js";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/lost-items", lostItemRoutes);
 
 app.get("/", (req, res) => {
   res.send("LostLink API is running 🚀");
